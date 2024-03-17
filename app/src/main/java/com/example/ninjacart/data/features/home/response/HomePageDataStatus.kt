5b3f612d6eb6e166aec88953a89fcada2c9dde4b
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2024-03-17 21:26:25 +0530

    #5 created custom progress bar

--- app/src/main/java/com/example/ninjacart/data/features/home/response/HomePageDataStatus.kt
+++ app/src/main/java/com/example/ninjacart/data/features/home/response/HomePageDataStatus.kt
@@ -3,5 +3,5 @@ package com.example.ninjacart.data.features.home.response
 sealed class HomePageDataStatus {
     data class Success(val home: Home?) : HomePageDataStatus()
     data class Error(val error: String) : HomePageDataStatus()
-    object Loading: HomePageDataStatus()
+    object Loading : HomePageDataStatus()
 }
