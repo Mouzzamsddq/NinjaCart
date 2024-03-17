4860d3304ef27ee26e2e7bd62eaf9d4179b41801
MouzzamSiddiqui07 <mouzzam.siddiqui@zopsmart.com>
2024-03-17 09:57:01 +0530

    inital commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.ninjacart"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.ninjacart"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val lifecycleVersion = "2.7.0"
+    val fragmentVersion = "1.6.2"
+    val retrofit = "2.9.0"
+    val converter = "2.9.0"
+
+    // live data and  view model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    // image downloading library
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    kapt("com.github.bumptech.glide:compiler:4.12.0")
+
+    // fragments
+    implementation("androidx.fragment:fragment-ktx:$fragmentVersion")
+
+    // retrofit and gson library
+    implementation("com.squareup.retrofit2:retrofit:$retrofit")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit")
+    implementation("com.squareup.retrofit2:converter-gson:$converter")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // logging interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // room components
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation("androidx.room:room-paging:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // hilt for dependency injection
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
+
+    // splash screen api
+    implementation("androidx.core:core-splashscreen:1.0.1")
+}
+
+kapt {
+    correctErrorTypes = true
+}
